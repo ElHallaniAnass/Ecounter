ab9acb2df1bf7e56a6b43e8f657ae17ce9de47c9
ElHallaniAnass <114657582+ElHallaniAnass@users.noreply.github.com>
2022-10-17 13:19:36 +0100

    Add files via upload

---
+++ js/graphe.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20', '21', '22','23', '24', '26', '27', '28', '29', '30' ],
+        datasets: [{
+            label: 'Exprimer en KWH ',
+            data: [2050, 1900, 2100, 2800, 1800, 2000, 2500, 2600, 2450, 1950, 2300, 2900],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
